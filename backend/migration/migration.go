251ebedb16061dee7db9d814cd7f059c82bc5544
Azusain <azusaings@gmail.com>
2024-07-24 00:54:00 +0800

    chore: update

--- backend/migration/migration.go
+++ backend/migration/migration.go
@@ -12,4 +12,7 @@ func Migrate() {
 	slog.Info(config.Version)
 	// 如果检查到存在这张表并且有这个变量，就运行 schema.sql 里的 SQL 语句自动构建数据库.
 	// 如果没有说明数据库没构建过，跳过这个函数就好.
+
+	// 另外数据库连接的那些变量可以和 config.Version 放在一起, 每次启动服务器前去那里填写好启动程序就好了
+	// 位置在 -> backend\config\config.go
 }
