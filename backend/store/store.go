43feac116f7c4a31d3e4a28cd73c130e643fa2da
Chen Hao Ming <2825493439@qq.com>
2024-07-27 22:57:58 +0800

    update Redis init and close funtion

--- backend/store/store.go
+++ backend/store/store.go
@@ -1,25 +1,43 @@
 package store
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log/slog"
 	"main/backend/config"
+
+	"github.com/go-redis/redis/v8"
 )
 
 var (
-	db *sql.DB
+	db  *sql.DB
+	rdb *redis.Client
 )
 
 func Init() error {
-	//connect to postgres
+	// connect to postgres
 	var err error
 	db, err = sql.Open("postgres", config.DatabaseUrl)
 	if err != nil {
-		slog.Error("Failed to connect to postgre database")
+		slog.Error("Failed to connect to Postgre database")
 		return err
 	}
-	slog.Info("successfully initialize postgres")
+	slog.Info("successfully initialize Postgres")
+
+	// connect to Redis
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     config.RedisAddr,
+		Password: config.RedisPassword,
+		DB:       config.RedisDB,
+	})
+	ctx := context.Background()
+	_, err = rdb.Ping(ctx).Result()
+	if err != nil {
+		slog.Error("Failed to connect Redis")
+		return err
+	}
+	slog.Info("successfully initialize Redis")
 	return nil
 }
 
@@ -30,5 +48,11 @@ func Close() error {
 		return err
 	}
 	slog.Info("db has been closed")
+	err = rdb.Close()
+	if err != nil {
+		slog.Error(fmt.Sprintf("Failed to close rdb: %s", err.Error()))
+		return err
+	}
+	slog.Info("rdb has been closed")
 	return nil
 }
--- go.mod
+++ go.mod
@@ -11,6 +11,9 @@ require (
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
