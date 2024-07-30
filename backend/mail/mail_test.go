6306961f7e8b9346bfb0e3b1bef509229107fb10
Chen Hao Ming <2825493439@qq.com>
2024-07-30 11:58:17 +0800

    docs:delete mail

--- backend/mail/mail_test.go
+++ backend/mail/mail_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestSendingMail(t *testing.T) {
 	mailClient := MailClient{
-		From:            "790874276@qq.com",
+		From:            "",
 		ServiceAuthCode: "wkktdkdalndhbdia",
 		ServiceDomain:   "smtp.qq.com",
 		ServicePort:     "587",
@@ -16,8 +16,8 @@ func TestSendingMail(t *testing.T) {
 
 	if err := mailClient.SendEmail(
 		[]string{
-			"790874276@qq.com",
-			"215117830@qq.com",
+			"",
+			"",
 		}, "Hello! This is azusaings.",
 		[]byte("Welcome to Revolution Fiesta"),
 		[]byte("<h1>Welcome! This is Revolution Fiesta</h1><p>Congruatulations!</p><p>所以看起来中文不会乱码吧</p>"),
